eabac37015cf0e2dea910f377ce8da2f60c5bae3
isocket@outlook.com <isocket@outlook.com>
2020-08-03 23:01:08 +0800

    gin test

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "hahahahahhaahahahah",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":9090") // listen and serve on 0.0.0.0:8080
 }
