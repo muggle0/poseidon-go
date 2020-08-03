8cce6e02e6d7e32405d3ca9c18adf221c434fbf5
isocket@outlook.com <isocket@outlook.com>
2020-08-03 23:23:03 +0800

    gin delete test

--- main.go
+++ main.go
@@ -15,7 +15,15 @@ func main() {
 	r.POST("/test1", func(context *gin.Context) {
 		fmt.Println(context.FullPath())
 		query := context.Query("name")
+		result, exit := context.GetPostForm("post")
+		if exit {
+			fmt.Println(result)
+		}
 		context.Writer.Write([]byte("message->" + query))
+	})
+	r.Handle("DELETE", "/test2/:id", func(context *gin.Context) {
+		param := context.Param("id")
+		context.Writer.Write([]byte(param))
 	})
 	r.Run(":9090") // listen and serve on 0.0.0.0:8080
 }
