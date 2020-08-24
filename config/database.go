7d14d167eaf09465a1c54859a1e46cb19d2bae8a
muggle <isocket@outlook.com>
2020-08-24 18:07:03 +0800

    我是节奏大师

--- config/database.go
+++ config/database.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	"github.com/jinzhu/gorm"
+	"sync"
+)
+
+var database *Database
+
+var mu sync.Mutex
+
+type Database struct {
+	Datatype string `yaml:"datatype"`
+	Hostname string `yaml:"hostname"`
+	Database string `yaml:"database"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Prefix   string `yaml:"prefix"`
+}
+
+func (database *Database) connection() {
+	db, _ := gorm.Open(database.Datatype, database.Username+":"+database.Password+"@/"+database.Database+"?charset=utf8&parseTime=True&loc=Local")
+	//全局禁用表复数
+	db.SingularTable(true)
+	// 设置连接池
+}
+
+func GetInstance() *Database {
+	if database == nil {
+		mu.Lock()
+		if database == nil {
+			// 初始化
+		}
+		mu.Unlock()
+
+	}
+	return database
+}
--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
 func main() {
 
-	gin.SetMode(gin.DebugMode)
-	new(config.Database).GetConnect()
-	defer config.DB.Close()
-	engine := gin.New()
-	(&route.Route{Engine: engine}).Run()
-	engine.Run(":8080")
+	/*	gin.SetMode(gin.DebugMode)
+		new(config.Database).GetConnect()
+		defer config.DB.Close()
+		engine := gin.New()
+		(&route.Route{Engine: engine}).Run()
+		engine.Run(":8080")*/
 }
