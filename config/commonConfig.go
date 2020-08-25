688738529028a1d9cb20bb80ca86e3085888adbe
isocket@outlook.com <isocket@outlook.com>
2020-08-25 23:42:03 +0800

    我是个废物

--- config/commonConfig.go
+++ config/commonConfig.go
@@ -9,15 +9,15 @@ import (
 	"path"
 )
 
-var properties map[interface{}]interface{}
+var Properties struct {
+	Database Database
+}
 
 func InitConfig() {
 	dir, _ := os.Getwd()
 	join := path.Join(dir, "/config/test.yml")
 	bytes, _ := ioutil.ReadFile(join)
 	fmt.Printf("读取项目配置，运行环境: %s\n", gin.Mode())
-	yaml.Unmarshal(bytes, &properties)
-	properties = properties[gin.Mode()].(map[interface{}]interface{})
-	fmt.Println(properties)
-	initDatabase(&properties)
+	yaml.Unmarshal(bytes, &Properties)
+
 }
--- config/database.go
+++ config/database.go
@@ -2,11 +2,9 @@ package config
 
 import (
 	"github.com/jinzhu/gorm"
-	"syscall"
 )
 
 var database *Database
-
 
 type Database struct {
 	Datatype string `yaml:"datatype"`
@@ -15,22 +13,22 @@ type Database struct {
 	Username string `yaml:"username"`
 	Password string `yaml:"password"`
 	Prefix   string `yaml:"prefix"`
+	DB       *gorm.DB
 }
 
-func (database *Database) connection() {
+func (database *Database) Connection() {
 	db, _ := gorm.Open(database.Datatype, database.Username+":"+database.Password+"@/"+database.Database+"?charset=utf8&parseTime=True&loc=Local")
 	//全局禁用表复数
+	database.DB = db
 	db.SingularTable(true)
+	db.Close()
 	// 设置连接池
 }
-func initDatabase(*map[interface{}]interface{}) {
 
+func GetInstance() *Database {
+	return database
 }
 
-func getInstance() (*Database,err error) {
-	if database==nil {
-		return
-	}
-
-	return database,nil;
+func (database *Database) Close() {
+	database.DB.Close()
 }
