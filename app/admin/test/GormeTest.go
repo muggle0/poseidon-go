186ea68c723ae8ef53e977c41dc1122535857ad8
yuanzening <yuanzn189@chinatelecom.cn>
2024-02-27 16:14:14 +0800

    gorm go 语法的讨论

--- app/admin/test/GormeTest.go
+++ app/admin/test/GormeTest.go
@@ -1,8 +1,52 @@
 package main
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 type Product struct {
 	gorm.Model
-	Code string `floa`
+	Code  string
+	Price uint
+}
+
+func main() {
+
+	//配置MySQL连接参数
+	username := "root"  //账号
+	password := "root"  //密码
+	host := "127.0.0.1" //数据库地址，可以是Ip或者域名
+	port := 3306        //数据库端口
+	Dbname := "gbg"     //数据库名
+	timeout := "10s"    //连接超时，10秒
+
+	//拼接dsn参数
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s", username, password, host, port, Dbname, timeout)
+	//连接MYSQL, 获得DB类型实例，用于后面的数据库读写操作。
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// 迁移 schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "P1", Price: 100})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                // 根据整型主键查找
+	db.First(&product, "code = ?", "p1") // 查找 code 字段值为 P1 的记录
+
+	// Update - 将 product 的 price 更新为 200
+	db.Model(&product).Update("Price", 200)
+	// Update - 更新多个字段
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete - 删除 product
+	db.Delete(&product, 1)
 }
